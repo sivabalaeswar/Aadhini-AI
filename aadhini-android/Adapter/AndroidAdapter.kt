d81ac2add3ac067cb6d6564a62d84e43a758c25a
sivabalaeswar <sivabalaeswar@gmail.com>
2026-07-30 21:33:25 +0530

    Create Adapter/AndroidAdapter.kt

---
+++ aadhini-android/Adapter/AndroidAdapter.kt
@@ -0,0 +1,14 @@
+package ai.aadhini.android.adapter
+
+import ai.aadhini.platform.adapter.Adapter
+
+class AndroidAdapter : Adapter {
+
+    override fun receive() {
+        TODO("Not yet implemented")
+    }
+
+    override fun send() {
+        TODO("Not yet implemented")
+    }
+} 
