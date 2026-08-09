2f93ddff433a70be426d50d563aa6e1294297305
sivabalaeswar <sivabalaeswar@gmail.com>
2026-08-10 05:09:16 +0530

    ui: add native Aadhini launcher activity

---
+++ aadhini-android/MainActivity.kt
@@ -0,0 +1,18 @@
+package ai.aadhini.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(AadhiniRootView(this))
+    }
+}
